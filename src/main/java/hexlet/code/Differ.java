7cc22700e15ba2cd9355872acaa918bfa6d7cce9
Oleg995 <oa.demin1305@omgau.org>
2022-06-02 02:25:35 +0600

    add

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -1,15 +1,27 @@
 package hexlet.code;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 
 
 public class DifferTest {
+
+    @Test
+    public void test() {
+        String path = "src/test/resources";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        System.out.println(absolutePath);
+
+        assertTrue(absolutePath.endsWith("src/test/resources"));
+    }
     @Test
     public void generateTest() throws IOException {
         String file = Files.readString(Paths.get("src/test/resources/example"));
